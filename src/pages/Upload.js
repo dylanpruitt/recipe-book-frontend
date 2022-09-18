51c8b773725a9884d8a482de76646a669c513d81
Dylan Pruitt <dpruitt0703@gmail.com>
2022-09-18 11:06:26 -0400

    Added Link import

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import DirectionList from "../components/DirectionList"
 import IngredientList from "../components/IngredientList"
 
