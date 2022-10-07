970e546ae4abc66611c14b3f0e06b18000d7621b
dylan pruitt <dpruitt0703@gmail.com>
2022-10-07 09:47:19 -0400

    Modified about and recipe pages to add qr code / prompt for users to turn phone

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,21 @@
+import recipeQR from "./recipe-qr-code.png"
+
 const About = () => {
     return (
         <article>
             <header className="w3-container w3-amber">
                 <h2>About</h2>
             </header>
-            <section className="w3-container">
-                <p>This website was created by Dylan Pruitt.</p>
+            <section className="w3-container w3-row-padding">
+                <section className="w3-half">
+                    <p>This website was created by Dylan Pruitt.</p>
+                    <p>A lot of recipe websites are crammed with ads, and aren't mobile friendly.</p>
+                    <p>I created this recipe book so that people can just get the bare essentials when looking for recipes.</p>
+                </section>
+                <section className="w3-half">
+                    <img src={recipeQR} className="w3-image" alt="QR code for mobile"></img>
+                    <p>Use this QR code to visit the website on your phone.</p>
+                </section>
             </section>
         </article>
     );
--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -4,9 +4,9 @@ import IngredientContainer from '.././components/IngredientContainer.js';
 import RecipeHeader from '.././components/RecipeHeader.js';
 
 function Recipe(props) {
-    const index  = props.index;
+    const index = props.index;
     const recipe = props.recipes[index];
-    
+
     var title = recipe.title;
     var description = recipe.description;
     var ingredients = recipe.ingredients;
@@ -15,6 +15,9 @@ function Recipe(props) {
     return (
         <div className="Recipe">
             <RecipeHeader name={title} description={description} />
+            <div class="w3-container w3-hide-medium w3-hide-large w3-blue">
+                <p>You can turn your phone to landscape mode to view recipes.</p>
+            </div>
             <section className="w3-row-padding">
                 <IngredientContainer ingredients={ingredients} />
                 <DirectionContainer directions={directions} />
